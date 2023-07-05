a4006c918f053897acf9337d2a5a61da3e0aaeb2
carlosandre-dev <andre.tjlba@gmail.com>
2023-07-04 22:33:28 -0300

    Exercise calculating the area, perimeter and diagonal of a rectangle

---
+++ HandsOn_Classes01/Program.cs
@@ -0,0 +1,13 @@
+﻿using HandsOn_Classes01;
+using System.Globalization;
+
+Rectangle rectangle = new Rectangle();
+
+Console.WriteLine("Informe a largura e altura do retângulo: ");
+rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("AREA: " + rectangle.Area().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("PERIMETRO: " + rectangle.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("DIAGONAL: " + rectangle.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
---
+++ HandsOn_Classes01/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿namespace HandsOn_Classes01
+{
+    internal class Rectangle
+    {
+        public double Width;
+        public double Height;
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return (2 * (Width + Height));
+        }
+
+        public double Diagonal()
+        {
+            double diagonal = (Width * Width) + (Height * Height);
+            return Math.Sqrt(diagonal);
+        }
+
+    }
+}
